282d18dd667ab456dc177b3eef0ae7a0f5419194
RussDeveloper <HBG(^gkfK2KjCZ#6@R>
2022-05-09 00:18:47 +0300

    Добавлена миграция и инициализирована база данных. Всё работает

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dict(ENGINE='django.db.backends.sqlite3', NAME=BASE_DIR / 'db.sqlite3')
 }
 
 
@@ -122,3 +119,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mysite/news/urls.py
@@ -0,0 +1,11 @@
+
+from  django.urls import path
+from news.views import index, test
+
+urlpatterns = [
+                path('', index),
+                path('test/', test),
+            ]
+
+
+
